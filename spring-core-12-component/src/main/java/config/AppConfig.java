e8462ee22cf8735029cbad5d67f802cfb27cfff1
Musluhan Çavuş <musluhan01@hotmail.com>
2022-12-12 11:28:59 +0300

    spring-core-12 Component UPDATED

--- spring-core-12-component/src/main/java/config/AppConfig.java
+++ spring-core-12-component/src/main/java/config/AppConfig.java
@@ -1,13 +1,20 @@
 package config;
 
+import model.DBConnection;
 import model.Database;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class AppConfig {
     @Bean
     public Database database(){
         return new Database("mysqlurl","username","passw");
+    }
+    @Bean
+    public DBConnection dbConnection(){
+        return new DBConnection(database());
     }
 }
--- spring-core-12-component/src/main/java/org/example/Main.java
+++ spring-core-12-component/src/main/java/org/example/Main.java
@@ -1,15 +1,18 @@
 package org.example;
 
+import config.AppConfig;
 import model.DBConnection;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
         ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        //applicationContext.getBean("DBConnection", DBConnection.class).toString();
-        System.out.println("database :" + applicationContext.getBean("DBConnection", DBConnection.class).getDatabase());
+        System.out.println("XML ************** :" + applicationContext.getBean("DBConnection", DBConnection.class).getDatabase());
 
+        ConfigurableApplicationContext applicationContext1 = new AnnotationConfigApplicationContext(AppConfig.class);
+        System.out.println("CONFİG *************" + applicationContext1.getBean(DBConnection.class).toString());
     }
 }
